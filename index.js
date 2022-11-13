f2084353041f0f966db8f460051e95fa2dc03176
K S S Prathyush <prathyushsunny@ip-192-168-29-247.ap-south-1.compute.internal>
2022-11-13 13:26:29 +0530

    Removed console.log statements

--- index.js
+++ index.js
@@ -4,12 +4,10 @@ $(document).ready(function(){
     const horizontalParentStartPosition = horizontalParent[0].offsetTop;
     const horizontalParentEndPosition = horizontalParentStartPosition + (horizontalParentStartPosition * Math.floor(heightVal/100));
     const horizontalParentHeight = horizontalParentEndPosition - horizontalParentStartPosition;
-    console.log(horizontalParentStartPosition, horizontalParentEndPosition, horizontalParentHeight);
 
     $(document).on('scroll', function(){
         const scrollPosition = Math.floor(window.scrollY);
         if (scrollPosition >= horizontalParentStartPosition && scrollPosition < horizontalParentEndPosition - horizontalParentStartPosition) {
-            // console.log(scrollPosition - horizontalParentStartPosition, horizontalParentHeight);
             const percentage = (((scrollPosition - horizontalParentStartPosition) / horizontalParentHeight) * 100);
             if (percentage <= 50) $('.horizontal-absolute-child').css('transform', `translateX(-${percentage}%)`);
         };
